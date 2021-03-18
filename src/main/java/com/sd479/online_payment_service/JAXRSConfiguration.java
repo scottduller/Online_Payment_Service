80a98925b5e70169d00222e3c2ecf5be227ba0ac
Scott Duller <scottduller@gmail.com>
2021-03-18 17:16:32 +0000

    Inital commit: Web layer inital complete (possible validation/conversions required)

---
+++ src/main/java/com/sd479/online_payment_service/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.sd479.online_payment_service;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
